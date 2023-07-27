244713b5539859c7f248cfd6ad4091f191189ddd
William Eckerleben <william.eckerleben@nts.com.py>
2023-07-27 15:52:31 -0400

    cambié twilio por messagebird

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import time
 from datetime import datetime
 import logging
+
+import messagebird
 import numpy as np
 import requests
-from twilio.rest import Client
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def get_menu(nombre, apellido):
-    url = "https://sheets.googleapis.com/v4/spreadsheets/1A8nrBoZuTCzcpcOAl3-d3i41-ZFOE9w9i7biwTXaryQ/values/'" + nombre + "%20" + apellido + "'!A1%3AE4"
+    url = "https://sheets.googleapis.com/v4/spreadsheets/1A8nrBoZuTCzcpcOAl3-d3i41-ZFOE9w9i7biwTXaryQ/values/'" + nombre + "%20" + apellido + "'!A1%3AE5"
     api_key = "AIzaSyCD2PkUgZkpn8KNW0_xr_BJOjR-wr3cODM"  # Replace this with your actual Google API Key
 
     params = {"key": api_key}
@@ -40,20 +41,17 @@ def convert_to_numpy_array(data):
 
 
 def send_message(msg, number):
-    account_sid = "AC1e97b7a0c7c568891f41d88b6ac998c5"
-    auth_token = "015e63603991b0257dc3a4249ac9a3f2"
-    twilio_phone_number = "+13203026795"
-    recipient_phone_number = number  # Replace this with the recipient's phone number
 
-    client = Client(account_sid, auth_token)
+    client = messagebird.Client('xkwIEg4lEpGxBZCeXfwSy8w9o')
 
     try:
-        client.messages.create(
-            body=msg,
-            from_=twilio_phone_number,
-            to=recipient_phone_number
+        client.message_create(
+            '+595982844853',
+            number,
+            msg,
+            {'reference': 'Foobar'}
         )
-        logging.info("WhatsApp message sent successfully!")
+        logging.info("Message sent successfully!")
     except Exception as e:
         logging.error(f"Error sending message: {e}")
 
@@ -69,15 +67,15 @@ if __name__ == "__main__":
         np_menu_ja = get_menu("Jessica", "Alvarez")
         index = datetime.now().isoweekday()
         date = str(datetime.now().day) + '/' + str(datetime.now().month) + '/' + str(datetime.now().year)
-        panambi_saludo = 'Panambi, acá te dejo mi menú, para que día y en donde lo recibiré.\n\n'
-        message_we = date + '\n\n' + panambi_saludo + '+--- WILLIAM ECKERLEBEN ---+\n'
-        message_ja = date + '\n\n' + panambi_saludo + '+---- JESSICA ALVAREZ -----+\n'
 
         now = datetime.now()
         logging.info(now)
         # Si son las 07:00 o 09:00 de lunes a viernes, envía el SMS
-        if (now.hour == 7 or now.hour == 9) and now.minute == 4:
+        if (now.hour == 7 or now.hour == 9) and now.minute == 0:
             if now.isoweekday() < 6:
+                panambi_saludo = 'Panambi, acá te dejo mi menú para hoy y en donde lo recibiré.\n\n'
+                message_we = date + '\n\n' + panambi_saludo + '+--- WILLIAM ECKERLEBEN ---+\n'
+                message_ja = date + '\n\n' + panambi_saludo + '+---- JESSICA ALVAREZ -----+\n'
                 for x in np_menu_we[index - 1]:
                     message_we += (x + '\n')
                 for x in np_menu_ja[index - 1]:
@@ -85,8 +83,12 @@ if __name__ == "__main__":
                 send_menu_we_ja(message_we, message_ja, '+595982844853')
                 send_menu_we_ja(message_we, message_ja, '+595982620856')
                 send_menu_we_ja(message_we, message_ja, '+595971133373')
+        # Si son las 19:00 de domingo a jueves, envía el SMS
         if now.hour == 19 and now.minute == 0:
             if now.isoweekday() != 5 and now.isoweekday() != 6:
+                panambi_saludo = 'Panambi, acá te dejo mi menú para mañana y en donde lo recibiré.\n\n'
+                message_we = date + '\n\n' + panambi_saludo + '+--- WILLIAM ECKERLEBEN ---+\n'
+                message_ja = date + '\n\n' + panambi_saludo + '+---- JESSICA ALVAREZ -----+\n'
                 for x in np_menu_we[index]:
                     message_we += (x + '\n')
                 for x in np_menu_ja[index]:
